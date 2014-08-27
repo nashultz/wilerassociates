4940358e05f12f0665c12aef9cd77d0e91398ebf
nashultz <nathons@systemsedgeonline.com>
2014-08-27 12:56:20 -0500

    sample2 update

--- public/views/sample2.blade.php
+++ public/views/sample2.blade.php
@@ -82,14 +82,44 @@
           </div><!--/.nav-collapse -->
       </div>
   </div>
-  <div class="main container">
-    <div class="row">
-      <div class="welcome">
-          <h1>Welcome</h1>
-          <p>We welcome you to Wiler & Associates Real Estate Services, Inc. Wiler & Associates offers more than two decades of commercial and residential real estate experience in the state of Texas. We bring together a collection of market savvy, customer centricity, and real estate experience to provide our customers with the greatest satisfaction.</p>
-          <p>At W&A we not only work closely with our clients to help facilitate the buying or selling process, we offer support from initial contact to closing and beyond. We strive to create value through the eyes of our clients, and pride ourselves on creating lifelong relationships by working hard to provide the best experience possible. Our licensed real estate team has earned a reputation for extraordinary service, integrity, and personal attention to our clients.</p>
-          <p>We look forward to having the opportunity to earn your business.</p>
+  <div class="welcome-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-9">
+          <div class="welcome">
+            <h1>Welcome</h1>
+            <p>We welcome you to Wiler & Associates Real Estate Services, Inc. Wiler & Associates offers more than two decades of commercial and residential real estate experience in the state of Texas. We bring together a collection of market savvy, customer centricity, and real estate experience to provide our customers with the greatest satisfaction.</p>
+            <p>At W&A we not only work closely with our clients to help facilitate the buying or selling process, we offer support from initial contact to closing and beyond. We strive to create value through the eyes of our clients, and pride ourselves on creating lifelong relationships by working hard to provide the best experience possible. Our licensed real estate team has earned a reputation for extraordinary service, integrity, and personal attention to our clients.</p>
+            <p>We look forward to having the opportunity to earn your business.</p>
+          </div>
         </div>
+        <div class="col-sm-3"> 
+          <div class="welcome">
+            <h3 class="search-title">Quick Search</h3>
+            <form class="">
+              <select class="form-control">
+                <option>Land - Commercial</option>
+                <option>Other - Commercial</option>
+                <option>Office/Warehouse</option>
+                <option>Retail</option>
+                <option>Hotel/Motel</option>
+              </select>
+              <div class="spacer-5"></div>
+              <input type="text" id="" name="" class="form-control" placeholder="Max Price" />
+              <div class="spacer-5"></div>
+              <input type="text" id="" name="" class="form-control" placeholder="Min Price" />
+              <div class="spacer-5"></div>
+              <input type="text" id="" name="" class="form-control" placeholder="County" />
+              <div class="spacer-5"></div>
+              <input type="submit" value="Search" class="btn btn-success btn-block" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="listings-container container">
+    <div class="row">
       <div class="listings-grid">
         <div class="listings">
           <div class="col-md-4">
@@ -126,7 +156,7 @@
                 </p>
               </div>
               <div class="viewlisting">
-                <button class="btn btn-primary btn-block">View Listing</button>
+                <button class="btn btn-warning btn-block">View Listing</button>
               </div>
             </div>
           </div>
@@ -145,7 +175,7 @@
                 </p>
               </div>
               <div class="viewlisting">
-                <button class="btn btn-primary btn-block">View Listing</button>
+                <button class="btn btn-danger btn-block">View Listing</button>
               </div>
             </div>
           </div>
