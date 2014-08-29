46ba67f7348ac662074523603d2ffb8f8c3d431b
nashultz <nathons@systemsedgeonline.com>
2014-08-28 23:05:22 -0500

    my files

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,8 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('homestead'),
-	'roman' => array('Misaki.local')
+	'roman' => array('Misaki.local'),
+	'nathon' => array('nashultz-p2')
 
 ));
 
